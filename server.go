9c816ea5c63d6a8f006c4fc29b522d4c0f3cc441
EndFroZen <Ef.chanachol@gmail.com>
2025-04-27 08:28:53 +0700

    hey hello this is v1.0 of cURL Generator

---
+++ server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	engine := html.New("./views", ".html")
+
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+	app.Use(logger.New())
+	app.Static("/css","./css")
+	app.Get("/",func (c *fiber.Ctx)error  {
+		return c.Render("poster",fiber.Map{
+			"title":"welcomechanachol",
+		})
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
